6d8c9be36a0a14874e2d2b52ae02dcb43a5a6624
Michael DeGraw <mkdegraw@gmail.com>
2017-02-03 16:50:39 -0600

    More comments, removed int current

--- mpi.c
+++ mpi.c
@@ -1,6 +1,3 @@
-// (c) 2017 電気の羊
-// (psst...actually it isn't)
-
 #include <mpi.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -15,18 +12,22 @@ int main(int argc, char * argv[]) {
 
 	int A[1200][1200];	// A is the buffer used by the main process
 	int B[2][1200];	// B is the buffer used by slave processes. There are two of them to support double buffering
-	int current = 0;
 	MPI_Request req[size-1][1200/size]; 	// each process has it's own request array, and each request is different
 	MPI_Status status[size - 1];	// each process also has it's own status
 
 	// these are the request and status objects used by the main processes
 	MPI_Request mreq;
 	MPI_Status mstatus;
+
 	int flag;	// flag is relatively worthless but I use it for MPI_Test, which allows
 						//  the program to print slave sums before the master process is completely
 						//  finished
 
-	// If rank is 0 then the process is the master process
+
+
+	//**************************************************************************//
+	//														MASTER PROCESS																//
+	//**************************************************************************//
 	if (rank == 0) {
 
 		// if the number of processes can't evenly divide 1200 then
@@ -45,8 +46,8 @@ int main(int argc, char * argv[]) {
 
 				// also do this as part of the "x" loop
 				// send to slave processes
-				if (current > 0) {
-					MPI_Isend(A[x], 1200, MPI_INT, current, 99, MPI_COMM_WORLD, &mreq);
+				if ((x % size) > 0) {
+					MPI_Isend(A[x], 1200, MPI_INT, (x % size), 99, MPI_COMM_WORLD, &mreq);
 					MPI_Test(&mreq, &flag, &mstatus);
 
 				// have the master process take a turn summing a row
@@ -58,33 +59,43 @@ int main(int argc, char * argv[]) {
 					}
 					printf("MASTER: %d\n", sum);
 				}
-
-				// current keeps track of which process should sum the current row
-				// this code cycles current through all the processes
-				current = (current + 1) % size;
 			}
+
+
+
+	//**************************************************************************//
+	//													SLAVE PROCESSES																	//
+	//**************************************************************************//
 	} else {
 
 		// relativeRank allows us to easily access the proper array (otherwise,
 		//  req[0] would just be wasted space)
 		int relativeRank = rank - 1;
 
+		// NOTE: if each process receives 200 requests, then there needs to be 201
+		//  steps in this for loop, hence the abnormal <=
 		for (int i = 0; i <= (1200/size); i++) {
 
 			// this program uses double-buffering, so current buffer cycles between
 			//  the two
 			int currentBuffer = i % 2;
+			// lastBuffer is used because we need to sum what was received on the
+			//  last cycle, not the current one
+			int lastBuffer = (i + 1) % 2;
 
-			// on the first call, simply start receiving data
+	//**************************************************************************//
+	//														FIRST TIME																		//
+	//Process simply begins receiving data, and then moves on to the next step	//
+	//**************************************************************************//
 			if (i == 0) {
 				MPI_Irecv(B[currentBuffer], 1200, MPI_INT, 0, 99, MPI_COMM_WORLD, &req[relativeRank][i]);
 
-			// on the last run, wait to receive the last row, then sum and print it
+	//**************************************************************************//
+	//															LAST TIME																		//
+	//		Process waits for the last receive to finish, then sums and prints		//
+	//**************************************************************************//
 			} else if (i == (1200/size)) {
 
-				// lastBuffer is used because we need to sum what was received on the
-				//  last cycle, not the current one
-				int lastBuffer = (i + 1) % 2;
 				int sum = 0;
 
 				// wait for the last request to complete
@@ -96,18 +107,17 @@ int main(int argc, char * argv[]) {
 				}
 				printf("SLAVE%d: %d\n", relativeRank, sum);
 
-			// otherwise, begin receiving a new row, then wait for the last row to finish
-			//  receiving, then sum it up and print it
+	//**************************************************************************//
+	//													EVERY OTHER TIME																//
+	// Process begins receiving data, then it waits for the previous receive to //
+	//	complete, then sums and prints the data received previously							//
+	//**************************************************************************//
 			} else {
 				int sum = 0;
-				int lastBuffer = (i + 1) % 2;
-
 				// receive data into the current buffer
 				MPI_Irecv(B[currentBuffer], 1200, MPI_INT, 0, 99, MPI_COMM_WORLD, &req[relativeRank][i]);
-
 				// wait for the previous request to finish
 				MPI_Wait(&req[relativeRank][i-1], &status[relativeRank]);
-
 				// sum the data contained in the previous buffer
 				for (int a = 0; a < 1200; a++) {
 					sum += B[lastBuffer][a];
@@ -118,6 +128,5 @@ int main(int argc, char * argv[]) {
 	}
 
 	MPI_Finalize();
-
 	return 0;
 }
