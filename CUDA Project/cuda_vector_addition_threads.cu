0afe34e20ee957659a9057bf3d4ca1b42ffd7bd9
Michael DeGraw <mkdegraw@gmail.com>
2017-04-20 10:54:55 -0500

    Code from first couple tutorials

---
+++ CUDA Project/cuda_vector_addition_threads.cu
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+#define N 512
+
+__global__ void add(int *a, int *b, int *c){
+  c[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
+}
+
+void random_ints(int *ar, int size){
+  int i;
+
+  for (i=0; i<size; i++){
+    ar[i] = rand();
+  }
+}
+
+int main(){
+  int *a, *b, *c;            // host copies of a, b, c
+  int *d_a, *d_b, *d_c;   // devices copies of a, b, c
+  int size = N * sizeof(int);
+  int i;
+
+  srand(time(NULL));
+
+  // Alloc space for device copies of a, b, c
+  cudaMalloc((void **)&d_a, size);
+  cudaMalloc((void **)&d_b, size);
+  cudaMalloc((void **)&d_c, size);
+
+  // Alloc space for host copies of a, b, c and setup input values
+  a = (int *)malloc(size); random_ints(a, N);
+  b = (int *)malloc(size); random_ints(b, N);
+  c = (int *)malloc(size);
+
+  // Copy inputs to device
+  cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+  // Launch add() kernel on GPU with N blocks
+//****************************************************************************//
+//    THIS IS THE DIFFERENCE BETWEEN THE BLOCKS VERSION AND THREAD VERSION    //
+//****************************************************************************//
+  add<<<1,N>>>(d_a, d_b, d_c);
+
+  // Copy result back to host
+  cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+  // Display results
+  for (i=0; i<N; i++){
+    cout << "Row " << i << ": " << a[i] << " + " << b[i] << " = " << c[i] << endl;
+  }
+
+  // Cleanup
+  free(a); free(b); free(c);
+  cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+
+  return 0;
+}
