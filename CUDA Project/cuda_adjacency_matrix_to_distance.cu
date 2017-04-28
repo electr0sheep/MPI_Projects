4f98f38c0665607a022a268d9102abf737ca0952
Michael DeGraw <mkdegraw@gmail.com>
2017-04-27 23:06:42 -0500

    Macros were successful, cleanup

--- CUDA Project/cuda_adjacency_matrix_to_distance.cu
+++ CUDA Project/cuda_adjacency_matrix_to_distance.cu
@@ -19,8 +19,6 @@ void initializeLinearGraph(int*);
 void printMatrix(int*, ofstream&);
 bool checkCircularResults(int*);
 bool checkLinearResults(int*);
-//__host__ __device__ void writeMatrix(int*, int, int, int);
-__host__ __device__ int readMatrix(int*, int, int);
 __global__ void calculateDistanceMatrix(int*, int);
 
 int main(){
@@ -85,20 +83,16 @@ void initializeCircularGraph(int *ar){
     for (i=0; i<N; i++){
       if (j == i+1 || i == j+1){
         WRITE_MATRIX(ar, i, j, 1);
-        // writeMatrix(ar, i, j, 1);
       } else if ((j == N-1 && i == 0) || (j == 0 && i == N-1)){
         WRITE_MATRIX(ar, i, j, 1);
-        // writeMatrix(ar, i, j, 1);
       } else{
         WRITE_MATRIX(ar, i, j, 999999);
-        // writeMatrix(ar, i, j, 999999);
       }
     }
   }
 
   for (i=0; i<N; i++){
     WRITE_MATRIX(ar, i, i, 0);
-    // writeMatrix(ar, i, i, 0);
   }
 }
 
@@ -109,17 +103,14 @@ void initializeLinearGraph(int *ar){
     for (i=0; i<N; i++){
       if (j == i+1 || i == j+1){
         WRITE_MATRIX(ar, i, j, 1);
-        // writeMatrix(ar, i, j, 1);
       } else {
         WRITE_MATRIX(ar, i, j, 999999);
-        // writeMatrix(ar, i, j, 999999);
       }
     }
   }
 
   for (i=0; i<N; i++){
     WRITE_MATRIX(ar, i, i, 0);
-    // writeMatrix(ar, i, i, 0);
   }
 }
 
@@ -137,20 +128,15 @@ void printMatrix(int *matrix, ofstream &myFile){
 
 bool checkCircularResults(int *matrix){
   int x, y, number, n;
-  // bool even;
   bool increment = true;
 
   number = 0;
 
-  // even = (N % 2) == 0;
-
   for (y=0; y<N; y++){
     for (x=0; x<N; x++){
       READ_MATRIX(matrix, x, y, n);
-      // if (number != readMatrix(matrix, x, y)){
       if (number != n){
         cout << "X: " << x << " Y: " << y << endl;
-        // cout << "number: " << number << " matrix: " << readMatrix(matrix, x, y) << endl;
         cout << "number: " << number << " matrix: " << n << endl;
         cout << "increment: " << increment << endl;
         return false;
@@ -175,15 +161,6 @@ bool checkCircularResults(int *matrix){
   return true;
 }
 
-// MAKE THESE MACROS
-__host__ __device__ void writeMatrix(int *matrix, int x, int y, int value){
-  matrix[x+(y*N)] = value;
-}
-
-__device__ int readMatrix(int *matrix, int x, int y){
-  return matrix[x+(y*N)];
-}
-
 __global__ void calculateDistanceMatrix(int *matrix, int k){
   // __shared__ int matrixCopy[N*N];
 
@@ -200,11 +177,9 @@ __global__ void calculateDistanceMatrix(int *matrix, int k){
           READ_MATRIX(matrix, i, j, num1);
           READ_MATRIX(matrix, i, k, num2);
           READ_MATRIX(matrix, k, j, num3);
-          // int minimum = min(readMatrix(matrix, i, j), readMatrix(matrix, i, k) + readMatrix(matrix, k, j));
           int minimum = min(num1, num2 + num3);
           // EXPERIMENTAL
           WRITE_MATRIX(matrix, i, j, minimum);
-          // writeMatrix(matrix, i, j, minimum);
           //writeMatrix(matrixCopy, i, j, minimum);
         }
       }
